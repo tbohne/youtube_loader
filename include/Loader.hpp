551ac4a31a5e4f9e6bf9f9dca86e25cf69dc57b2
tbohne <tbohne@uos.de>
2018-07-08 21:25:00 +0200

    added documentation for the loader / minor changes

--- include/Loader.hpp
+++ include/Loader.hpp
@@ -1,5 +1,5 @@
-#define MEGABYTE 1048576;
-/* Unicode char representing a filled "block". */
+#define MEBIBYTE 1048576;
+// Unicode chars representing a filled block.
 #define BLOCK "\u2588\u2588"
 #define POINTS "...................."
 
@@ -10,17 +10,49 @@ class Loader
 {
 
 public:
+    /**
+     * @brief Creates an instance of the loader.
+     * @param CurlHandler* handler - pointer to the curl handler
+     */
     Loader(CurlHandler* handler);
     ~Loader();
 
+    /**
+     * @brief Checks whether the given filename refers to an existing file.
+     * @param string filename - the filename to be checked
+     * @return bool - true if it exists
+     */
     bool is_existing(std::string filename);
+
+    /**
+     * @brief Checks whether the output dir exists and creates it if not.
+     */
     void check_output_dir();
+
+    /**
+     * @brief Downloads and saves the video. It checks for duplicate downloads and
+     *        renames them if necessary. (The originals aren't overwritten - incremented index).
+     * @param const char* url - the url to download from
+     * @param string filename - the name of the file to save the video in
+     * @param string extension - the file's extension (usually mp4)
+     * @param CURL* curl - the curl instance
+     */
     void download_video(const char* url, std::string filename, std::string extension, CURL* curl);
 
 private:
+    // Member variable that stores a pointer to the curl handler.
     CurlHandler* handler;
 };
 
+/**
+ * @brief Callback function that visualizes the current download progress.
+ * @param void* clientp - progress callback pointer
+ * @param curl_off_t dltotal - the total size of the download
+ * @param curl_off_t dlnow - the size downloaded so far
+ * @param curl_off_t ultotal - the total size of the upload
+ * @param curl_off_t ulnow - the size uploaded so far
+ * @return int error code - 0 means everything worked as expected
+ */
 static int download_progress(
     void* clientp,
     curl_off_t dltotal,
--- src/Loader.cpp
+++ src/Loader.cpp
@@ -10,21 +10,18 @@ Loader::Loader(CurlHandler* handler)
 
 Loader::~Loader() {}
 
-/**
- * Callback function that visualizes the current download progress.
- */
 static int download_progress(
     void* clientp, curl_off_t dltotal, curl_off_t dlnow, curl_off_t ultotal, curl_off_t ulnow
 )
 {
-    /* Prevents division by 0. */
+    // Prevents division by 0.
     if (dltotal == 0) { return 0; }
 
-    /* Stores the current download progress in percent. */
+    // Stores the current download progress in percent.
     float progress = ((float)dlnow / (float)dltotal) * 100.0;
 
-    /* The string blocks represents the current download progress
-       in blocks to visualize it with a download status bar. */
+    // The variable blocks represents the current download progress
+    // in 'blocks' to visualize it with a download status bar.
     std::string points = POINTS;
     std::string blocks = "";
 
@@ -34,40 +31,34 @@ static int download_progress(
         points.erase(0, 2);
     }
 
-    /* dlnow stores the progress in bytes - convert to MB. */
-    float dlnow_mb = (float)dlnow / (float)MEGABYTE;
+    // dlnow stores the progress in bytes - convert to mebibytes.
+    float dlnow_mb = (float)dlnow / (float)MEBIBYTE;
 
-    /* std::fixed        --> Modifies the default formatting for floating-point input / output.
-       std::setprecision --> Sets the decimal precision to be used to format floating-point
-                             values on output operations (2 digits)
-       "\r"              --> Carriage Return - moves the cursor to the beginning of the line
-                             without advancing to the next line
-       I'm using the carriage return to be able to override the current line again and again
-       to have a nice and clean visualization of the download progress without printing
-       in a new line each iteration. */
+    // std::fixed        --> Modifies the default formatting for floating-point input / output.
+    // std::setprecision --> Sets the decimal precision to be used to format floating-point
+    //                       values on output operations (2 digits)
+    // "\r"              --> Carriage Return - moves the cursor to the beginning of the line
+    //                       without advancing to the next line
+    // I'm using the carriage return to be able to override the current line again and again
+    // to have a nice and clean visualization of the download progress without printing
+    // in a new line each iteration.
     std::cout << "  " << blocks << points << "  " << std::fixed << std::setprecision(2)
     << progress << "% - " << std::fixed << std::setprecision(2) << dlnow_mb << "MB" << "\r";
 
-    /* For output streams (and for update streams on which the last operation
-       was output), writes any unwritten data from the stream's buffer to the
-       associated output device.
-       --> In this case it stops the cursor from "blinking" through the output line. */
+    // For output streams (and for update streams on which the last operation
+    // was output), writes any unwritten data from the stream's buffer to the
+    // associated output device.
+    // --> In this case it stops the cursor from "blinking" through the output line.
     fflush(stdout);
     return 0;
 }
 
-/**
- * Checks whether the given filename refers to an existing file.
- */
 bool Loader::is_existing(std::string filename)
 {
     std::ifstream infile(filename);
     return infile.good();
 }
 
-/**
- * Checks whether the output dir exists and creates it if not.
- */
 void Loader::check_output_dir()
 {
     if (!boost::filesystem::exists("../output/"))
@@ -76,24 +67,17 @@ void Loader::check_output_dir()
     }
 }
 
-/**
- * Downloads and saves the video.
- * It checks for duplicate downloads and renames them if necessary.
- * (The originals aren't overwritten - incremented index).
- */
 void Loader::download_video(const char* url, std::string filename, std::string extension, CURL* curl)
 {
     filename += "." + extension;
-    /* The downloaded videos should be stored in the output dir. */
+    // The downloaded videos should be stored in the output dir.
     filename = "../output/" + filename;
-
     check_output_dir();
 
-    /* Appends an index with the number of the copy of the same file to the filename. */
+    // Appends an index with the number of the copy of the same file to the filename.
     if (is_existing(filename))
     {
         std::cout << "The file " << filename << " already exists." << std::endl;
-
         int idx = 1;
         std::string tmp_filename = filename;
         while (is_existing(tmp_filename))
@@ -104,17 +88,14 @@ void Loader::download_video(const char* url, std::string filename, std::string e
         }
         filename = tmp_filename;
     }
-
     FILE* file;
     file = fopen(filename.c_str(), "w");
-
     if (file == NULL)
     {
         std::cout << "Error while opening " << filename << std::endl;
         std::exit(1);
     }
-
-    /* download_progess is a pointer to the function. */
+    // download_progess is a pointer to the function.
     this->handler->write_file(curl, url, file, filename, download_progress);
 
     std::cout << std::endl << "Done!" << std::endl;
