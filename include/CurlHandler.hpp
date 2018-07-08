db4da0112cd18106fc983407840428b73351d79c
tbohne <tbohne@uos.de>
2018-07-08 21:01:01 +0200

    added documentation for the curl handler

--- include/CurlHandler.hpp
+++ include/CurlHandler.hpp
@@ -1,6 +1,9 @@
 namespace youtube_loader
 {
 
+/**
+ * @brief Handles the CURL operations.
+ */
 class CurlHandler
 {
 
@@ -8,6 +11,14 @@ public:
     CurlHandler();
     ~CurlHandler();
 
+    /**
+     * @brief Writes the downloaded video to the file.
+     * @param CURL* curl - the curl instance
+     * @param const char* url - the url to work with
+     * @param string filename - the name of the file to write to
+     * @param int (*download_progress)(void*, curl_off_t, curl_off_t, curl_off_t, curl_off_t)
+     *        - function pointer to download_progress
+     */
     void write_file(
         CURL* curl,
         const char* url,
@@ -16,9 +27,30 @@ public:
         int (*download_progress)(void*, curl_off_t, curl_off_t, curl_off_t, curl_off_t)
     );
 
+    /**
+     * @brief Receives the content of the response to the url.
+     * @param CURL* curl - the curl instance
+     * @param const char* url - the url to work with
+     * @param string* response - pointer to the response string
+     */
     void receive_url_response(CURL* curl, const char* url, std::string* response);
 };
 
+/**
+ * @brief Writes the received data.
+ *        This callback function gets called by libcurl as soon as there is data
+ *        received that needs to be saved.
+ *        The callback function will be passed as much data as possible in all
+ *        invokes, but you must not make any assumptions. It may be one byte,
+ *        it may be thousands. Your callback should return the number of bytes
+ *        actually taken care of. If that amount differs from the amount passed
+ *        to your callback function, it'll signal an error condition to the library.
+ * @param char* buffer - the delivered data
+ * @param size_t size - the size of one element
+ * @param size_t nmemb - the number of elements
+ * @param string* response - pointer to the response string
+ * @return size_t - the number of bytes taken care of
+ */
 static size_t write_callback(char* buffer, size_t size, size_t nmemb, std::string* response);
 
 } // namespace youtube_loader
--- src/CurlHandler.cpp
+++ src/CurlHandler.cpp
@@ -6,28 +6,12 @@ namespace youtube_loader
 CurlHandler::CurlHandler() {}
 CurlHandler::~CurlHandler() {}
 
-/**
- * Writes the received data.
- * This callback function gets called by libcurl as soon as there is data
- * received that needs to be saved. buffer points to the delivered data,
- * and the size of that data is size multiplied with nmemb.
- * The callback function will be passed as much data as possible in all
- * invokes, but you must not make any assumptions. It may be one byte,
- * it may be thousands. Your callback should return the number of bytes
- * actually taken care of. If that amount differs from the amount passed
- * to your callback function, it'll signal an error condition to the library.
- */
 static size_t write_callback(char* buffer, size_t size, size_t nmemb, std::string* response)
 {
     *response += buffer;
     return size * nmemb;
 }
 
-/**
- * Writes to the file.
- * int (*download_progress)(void*, curl_off_t, curl_off_t, curl_off_t, curl_off_t)
- * return_type (*func_name)(parameter_types) --> function pointer to download_progress().
- */
 void CurlHandler::write_file(
     CURL* curl,
     const char* url,
@@ -36,19 +20,18 @@ void CurlHandler::write_file(
     int (*download_progress)(void*, curl_off_t, curl_off_t, curl_off_t, curl_off_t)
 )
 {
-    /* Resets all options of a libcurl session handle. */
+    // Resets all options of a libcurl session handle.
     curl_easy_reset(curl);
 
-    /* https://curl.haxx.se/libcurl/c/progressfunc.html */
+    // https://curl.haxx.se/libcurl/c/progressfunc.html
     if (curl)
     {
         /************** Sets options for a curl easy handle *******************/
-        /* Sets the url to work on. */
+        // Sets the url to work with.
         curl_easy_setopt(curl, CURLOPT_URL, url);
-        /* Sets the data to be written to the file. */
+        // Sets the data to be written to the file.
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
-        /* Using the download_progress function:
-           There are a couple of more options to set if necessary (works for now). */
+        // Using the download_progress function:
         curl_easy_setopt(curl, CURLOPT_XFERINFOFUNCTION, download_progress);
         curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);
         /**********************************************************************/
@@ -72,27 +55,22 @@ void CurlHandler::write_file(
     }
 }
 
-/**
- * Receives the content of the response to the url currently working on.
- * http://www.cplusplus.com/forum/unices/45878/
- */
 void CurlHandler::receive_url_response(CURL* curl, const char* url, std::string* response)
 {
-    /* Resets all options of a libcurl session handle. */
+    // Resets all options of a libcurl session handle.
     curl_easy_reset(curl);
 
     if (curl)
     {
         /************** Sets options for a curl easy handle *******************/
-        /* Sets URL to work on */
+        // Sets the url to work with.
         curl_easy_setopt(curl, CURLOPT_URL, url);
-        /* Callback for writing data */
+        // Callback for writing the data.
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, response);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &write_callback);
         /**********************************************************************/
 
-        /* Performs a blocking file transfer.
-           Returns a curl error code. */
+        // Performs a blocking file transfer and returns a curl error code.
         CURLcode curl_code = curl_easy_perform(curl);
 
         if (curl_code != CURLE_OK)
